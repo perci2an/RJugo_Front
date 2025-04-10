d37a04d2a1faec901888e27dce7e23f6393f60bf
perci2an <psmim1216@naver.com>
2025-04-10 16:06:31 +0900

    🩹Simple_fix: 샘플 단어 변경

--- src/app/_components/Header/Menu.jsx
+++ src/app/_components/Header/Menu.jsx
@@ -4,15 +4,17 @@ export default function Menu() {
   return (
     <div className="absolute right-0 top-[70px] w-[300px] p-1 bg-white border-[3px] border-gray-300 rounded-[30px] z-50">
       <div className="bg-white rounded-[27px] p-6 border-[1.5px] border-dashed border-gray-200 grid grid-cols-3 gap-4 text-center">
-        <MenuItem src="/img/egg.png" label="Gmail" />
-        <MenuItem src="/img/egg.png" label="Drive" />
-        <MenuItem src="/img/egg.png" label="YouTube" />
-        <MenuItem src="/img/egg.png" label="Gmail" />
-        <MenuItem src="/img/egg.png" label="Drive" />
-        <MenuItem src="/img/egg.png" label="YouTube" />
-        <MenuItem src="/img/egg.png" label="Gmail" />
-        <MenuItem src="/img/egg.png" label="Drive" />
-        <MenuItem src="/img/egg.png" label="YouTube" />
+        <MenuItem src="/img/egg.png" label="일자리" />
+        <MenuItem src="/img/egg.png" label="주거" />
+        <MenuItem src="/img/egg.png" label="진로" />
+        <MenuItem src="/img/egg.png" label="교육" />
+        <MenuItem src="/img/egg.png" label="생활" />
+        <MenuItem src="/img/egg.png" label="대외활동" />
+        <MenuItem src="/img/egg.png" label="예술" />
+        <MenuItem src="/img/egg.png" label="사회" />
+        <MenuItem src="/img/egg.png" label="커뮤니티" />
+        <MenuItem src="/img/egg.png" label="공간" />
+        <MenuItem src="/img/egg.png" label="상담" />
       </div>
     </div>
   );
