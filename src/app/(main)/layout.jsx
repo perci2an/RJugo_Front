2b0811e4708a631bcd3d121dcb687fa2a5e395f8
perci2an <psmim1216@naver.com>
2025-04-10 14:23:45 +0900

    🎨Improve structure: 헤더 초기 세팅

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,15 +1,13 @@
-import MainBanner from "../_components/MainBanner";
-
-export const metadata = {
-  title: "RJugo",
-  description: "youth information",
-};
+import Header from "../_components/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <main className="flex flex-col items-center">{children}</main>
+        <main className="flex flex-col items-center w-[1280px] mx-auto py-5 px-10">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
