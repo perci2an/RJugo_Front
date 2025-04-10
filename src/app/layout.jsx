0dc3d5d1820c3b6ffb669f1f567e9d08dbc7d64b
perci2an <psmim1216@naver.com>
2025-04-10 13:40:48 +0900

    🎨Improve structure: MainBanner 컴포넌트 호출 위치 변경
    - 초기 페이지는 구조가 다르기 때문에 최상단 page.jsx에서 호출하도록 변경

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,9 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="w-[1280px] mx-auto pt-[55px] pb-[50px] px-0">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import MainBanner from "./_components/MainBanner";
+
+export default function Home() {
+  return (
+    <main className="w-[1280px] mx-auto pt-[55px] pb-[50px] px-0">
+      <MainBanner />
+    </main>
+  );
+}
