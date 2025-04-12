81297829a98e816ad06060f8d7e2d1ef7f9ad4c3
perci2an <psmim1216@naver.com>
2025-04-12 12:21:09 +0900

    💄Design: 배경 색 적용
    - gradient 클래스를 활용한 그라데이션 적용
    - 초기 페이지(고급, 차분)와 메인 페이지(부드러움, 은은) 그라데이션 색상 다름

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -4,9 +4,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <main className="flex flex-col items-center w-[1280px] mx-auto py-5 px-16">
-          <Header />
-          {children}
+        <main className="bg-gradient-to-br from-[#ffffff] via-[#f7f7f7] to-[#e1e1e1]">
+          <div className="flex flex-col items-center w-[1280px] mx-auto py-5 px-16">
+            <Header />
+            {children}
+          </div>
         </main>
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,10 @@ import MainBanner from "./_components/MainBanner";
 
 export default function Home() {
   return (
-    <main className="w-[1280px] mx-auto pt-[55px] pb-[50px] px-0">
-      <MainBanner />
+    <main className="bg-gradient-to-br from-[#fefcfb] via-[#ece9f1] to-[#d9e1f2]">
+      <div className="w-[1280px] mx-auto pt-[55px] pb-[50px] px-0">
+        <MainBanner />
+      </div>
     </main>
   );
 }
