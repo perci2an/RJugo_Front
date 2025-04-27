c0525b2f55fcd978ceb384fad5264ae33379279d
perci2an <psmim1216@naver.com>
2025-04-27 22:37:45 +0900

    🩹Simple_fix: 금융 페이지 카드 제목 변경

--- src/app/(main)/finance/page.jsx
+++ src/app/(main)/finance/page.jsx
@@ -42,7 +42,7 @@ export default function Home() {
       bgImage: "/img/CardImage/Finance/7.png",
     },
     {
-      title: "자산형성 지원 프로그램",
+      title: "온라인 예금중계 서비스",
       location: "부산시",
       bgImage: "/img/CardImage/Finance/8.jpeg",
     },
